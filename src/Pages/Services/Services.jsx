3fe924b2d509f749ed7b784742c2b2be15717d27
boburbekburkhonov <zafaroviccc@gmail.com>
2023-05-16 14:01:08 +0500

    feat: services

--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -1,8 +1,81 @@
 import React from "react";
+import service from "../../assets/images/services.png";
 import "./Services.css";
 
 const Services = () => {
-  return <div></div>;
+  return (
+    <section className="services">
+      <div className="container">
+        <h2 className="services-heading">Xizmatlar</h2>
+
+        <ul className="list-unstyled m-0 services-list">
+          <li className="services-item">
+            <img
+              className="services-img"
+              src={service}
+              alt="service"
+              width="300"
+              height="220"
+            />
+            <p className="services-desc m-0">Quyosh panellari</p>
+          </li>
+
+          <li className="services-item">
+            <img
+              className="services-img"
+              src={service}
+              alt="service"
+              width="300"
+              height="220"
+            />
+            <p className="services-desc m-0">Quyosh panellari</p>
+          </li>
+          <li className="services-item">
+            <img
+              className="services-img"
+              src={service}
+              alt="service"
+              width="300"
+              height="220"
+            />
+            <p className="services-desc m-0">Quyosh panellari</p>
+          </li>
+
+          <li className="services-item">
+            <img
+              className="services-img"
+              src={service}
+              alt="service"
+              width="300"
+              height="220"
+            />
+            <p className="services-desc m-0">Quyosh panellari</p>
+          </li>
+          <li className="services-item">
+            <img
+              className="services-img"
+              src={service}
+              alt="service"
+              width="300"
+              height="220"
+            />
+            <p className="services-desc m-0">Quyosh panellari</p>
+          </li>
+
+          <li className="services-item">
+            <img
+              className="services-img"
+              src={service}
+              alt="service"
+              width="300"
+              height="220"
+            />
+            <p className="services-desc m-0">Quyosh panellari</p>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
 };
 
 export default Services;
